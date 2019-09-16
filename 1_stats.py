601a06a796198bdc914c4cd5e89ae70b0e99463d
wangzhengbang <55188762+wangzhengbang@users.noreply.github.com>
2019-09-16 13:13:37 +0800

    Update 1_stats.py

--- 1_stats.py
+++ 1_stats.py
@@ -4,6 +4,6 @@ import time
 import json
 
 
-output = [{"endpoint": "monitor-test-centos", "tags": "", "timestamp": int(time.time()), "metric": "agent.cpu", "value": 1.8, "counterType": "GAUGE", "step": 60}]
+output = [{"endpoint": "open-falcon-server", "tags": "", "timestamp": int(time.time()), "metric": "agent.cpu", "value": 1.8, "counterType": "GAUGE", "step": 60}]
 
 print  json.dumps(output)
