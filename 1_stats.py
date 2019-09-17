ff31f124c94fef98f87727f2e21c700b99a1a9a6
wangzhengbang <55188762+wangzhengbang@users.noreply.github.com>
2019-09-17 13:31:08 +0800

    Delete 1_stats.py

--- 1_stats.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import time
-import json
-
-
-output = [{"endpoint": "open-falcon-server", "tags": "", "timestamp": int(time.time()), "metric": "agent.cpu", "value": 1.8, "counterType": "GAUGE", "step": 60}]
-
-print  json.dumps(output)
